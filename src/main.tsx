f3414ed2271c3d9ffde2fb082d56a97dc78e5024
Cyfroni <karolplg@gmail.com>
2022-12-09 21:15:35 +0100

    typesafe api

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,26 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import Root, { loader as rootLoader } from "./routes/root";
+import Root from "./routes/root";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
-    loader: rootLoader,
   },
 ]);
 
+const client = new ApolloClient({
+  uri: import.meta.env.VITE_GRAPHQL_URL,
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ApolloProvider client={client}>
+      <RouterProvider router={router} />
+    </ApolloProvider>
   </React.StrictMode>
 );
--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,7 +1,29 @@
-export const loader = () => {
-  return [1, 2, 3];
-};
+import { useQuery } from "@apollo/client";
+import { gql } from "../__generated__/gql";
+
+const GET_Q = gql(`
+  query get{
+    albums {
+      data {
+        id
+        title
+      }
+    }
+  }
+`);
 
 export default function Root() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const { loading, error, data } = useQuery(GET_Q);
+
+  if (loading) return <div>loading</div>;
+
+  if (error) return <div>error</div>;
+
+  return (
+    <h1 className="text-3xl font-bold underline">
+      {data?.albums?.data?.slice(0, 10).map((album) => (
+        <div key={album?.id}>{album?.title}</div>
+      ))}
+    </h1>
+  );
 }
